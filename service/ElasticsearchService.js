bf25d1591996c6d107f042079a1b54d1170243a4
Matthias Karl <matthias.karl@mydays.de>
2018-09-18 10:44:17 +0200

    Add performnace metrics

--- service/ElasticsearchService.js
+++ service/ElasticsearchService.js
@@ -41,7 +41,32 @@ const doctype = {
         "timestamp": { "type": "date", "format": "epoch_millis" },
         "userAgent": { "type": "text" },
         "page": { "type": "text" },
-        "performance":{ "type": "float" }
+        "performance":{ "type": "float" },
+        "bestpractice": { "type": "float" },
+        "seo": { "type": "float" },
+        "performance-first-contentful-paint": { "type": "float" },
+        "performance-first-meaningful-paint": { "type": "float" },
+        "performance-speed-index": { "type": "float" },
+        "performance-interactive": { "type": "float" },
+        "performance-first-cpu-idle": { "type": "float" },
+        "performance-estimated-input-latency": { "type": "float" },
+        "performance-render-blocking-resources": { "type": "float" },
+        "performance-render-blocking-resources-data": { "type": "array" },
+        "performance-uses-responsive-images": { "type": "float" },
+        "performance-uses-responsive-images-data": { "type": "array" },
+        "performance-offscreen-images": { "type": "float" },
+        "performance-offscreen-images-data": { "type": "array" },
+        "performance-unminified-css": { "type": "float" },
+        "performance-unminified-css-data": { "type": "array" },
+        "performance-unminified-js": { "type": "float" },
+        "performance-unminified-js-data": { "type": "array" },
+        "performance-unused-css-rules": { "type": "integer" }, //save displayValue
+        "performance-unused-css-rules-data": { "type": "array" },
+        "performance-uses-optimized-images": { "type": "integer"}, //save displayValue
+        "performance-uses-optimized-images-data": { "type": "array"},
+        "performance-uses-webp-images": { "type": "integer" },
+        "performance-uses-rel-preconnect": { "type": "float" },
+        "performance-time-to-first-byte": { "type": "float" },
     }
 };
 
